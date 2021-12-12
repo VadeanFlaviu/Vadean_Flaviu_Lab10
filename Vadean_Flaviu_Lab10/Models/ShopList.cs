d0e0f371f6553c9ff8b876057f248234f1ee4138
VadeanFlaviu <flaviu@metagamestudio.com>
2021-12-12 11:12:43 +0200

    Add project files.

---
+++ Vadean_Flaviu_Lab10/Models/ShopList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using SQLite;
+
+namespace Vadean_Flaviu_Lab10.Models
+{
+    public class ShopList
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
